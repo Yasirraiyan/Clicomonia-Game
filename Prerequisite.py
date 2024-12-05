37bcf9a4d55da38a4ae6c2e473147a76555da26b
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-05 22:12:35 +0600

    Create Prerequisite.py

---
+++ Prerequisite.py
@@ -0,0 +1,12 @@
+import random;
+egg=['red','green','blue','yellow','black']
+choice=random.choie(egg);
+user=input("Enter choice:");
+int level=0;
+score=0;
+attempt=5;
+time=10;
+motion=0;
+challange=10;
+bool click=False;
+bool allsuccess=False/
