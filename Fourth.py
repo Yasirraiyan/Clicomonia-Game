8463064f91ab8c9d88315ccda78b4adfd893e673
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-05 22:16:08 +0600

    Create Fourth.py

---
+++ Fourth.py
@@ -0,0 +1,22 @@
+ def fourth_step(level,choice):
+    if choice in egg:
+        print("choice valid")
+    else:
+        print("choice invalid!")
+        while attempt>0&&!click :
+            if choice in egg:
+                print("Fourth step done.")
+                score+=4;
+                attempt--;
+                time/16;
+                motion+=200;
+                challange+=40;
+                level+=4;
+                if attempt ==0 and time==0:
+                    print("First step finish")
+                    if score==5:
+                        print("Success")
+                          score==300:
+                        print("Win.go next step.")
+                        else:
+                            print("Fail.Finish")
