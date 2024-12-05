dfc9160a20f2ffa66d60db38cc9918eddc164aff
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-05 22:15:23 +0600

    Create Third.py

---
+++ Third.py
@@ -0,0 +1,20 @@
+def third_step(level,choice):
+    if choice in egg:
+        print("choice valid")
+    else:
+        print("choice invalid!")
+        while attempt>0&&!click :
+            if choice in egg:
+                print("First step done.")
+                score+=3;
+                attempt--;
+                time/8;
+                motion+=150;
+                challange+=30;
+                level+=3;
+                if attempt ==0 and time==0:
+                    print("Third step finish")
+  if score==250:
+                        print("Win.go next step.")
+                        else:
+                            print("Fail.Finish")
