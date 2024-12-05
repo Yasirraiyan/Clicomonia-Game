5329e2dfb6d0155ff585feecfc60f20a152e8d03
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-05 22:16:45 +0600

    Create Fifth.py

---
+++ Fifth.py
@@ -0,0 +1,22 @@
+def fifth_step(level,choice):
+    if choice in egg:
+        print("choice valid")
+    else:
+        print("choice invalid!")
+        while attempt>0&&!click :
+            if choice in egg:
+                print("Fifth step done.")
+                score+=5;
+                attempt--;
+                time/32;
+                motion+=250;
+                challange+=50;
+                level+=5;
+                if attempt ==0 and time==0:
+                    print("First step finish")
+                    if score==5:
+                        print("Success")
+  score==350:
+                        print("Win.go next step.")
+                        else:
+                            print("Fail.Finish")
